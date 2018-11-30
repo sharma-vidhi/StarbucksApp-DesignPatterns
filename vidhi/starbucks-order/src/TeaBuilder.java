26b9f473c3c39ee3f78f921a3d55fe21786dbecd
sharma-vidhi <vidhi.sharma@sjsu.edu>
2018-11-29 21:11:12 -0800

    modified builder pattern and added screens for order,coffee,tea

--- vidhi/starbucks-order/src/TeaBuilder.java
+++ vidhi/starbucks-order/src/TeaBuilder.java
@@ -1,3 +1,4 @@
+//tea screen
 // Concrete Builder to build tea
 class TeaBuilder extends StarbucksBuilder {
 	public void buildSize() {
@@ -8,5 +9,19 @@ class TeaBuilder extends StarbucksBuilder {
 	public void buildDrink() {
 		starbucks.setDrink("tea");
 		System.out.println("tea");
+	}
+
+	@Override
+	public void buildMilk() {
+		starbucks.setMilk("2%Milk");
+		System.out.println("2% Milk");
+		
+	}
+
+	@Override
+	public void buildFalvour() {
+		starbucks.setFalvour("Chai Tea Latte");
+		System.out.println("Chai Tea Latte");
+		
 	}
 }
